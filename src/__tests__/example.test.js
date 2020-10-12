fb54ee62bb2195545de8632c0e36c19ca3ec8c07
Baiyuan Wang <bywang@thoughtworks.com>
2020-10-12 20:26:51 +0800

    change test case style to describe it

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,5 +1,7 @@
 import sum from "../sum";
 
-test('adds 1 + 2 to equal 3', () => {
+describe("sum", () => {
+  it('should return 3 when give 1 + 2', () => {
     expect(sum(1, 2)).toBe(3);
-});
+  });
+})
